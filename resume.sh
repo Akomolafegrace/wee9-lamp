a24385669a5b811424902ef1c513b52243ebafd0
Akomolafegrace <Akomolafegrace06@gmail.com>
2025-06-30 19:09:29 -0400

    Route534

--- resume.sh
+++ resume.sh
@@ -1,8 +1,15 @@
-sudo yum update -y
-sudo yum install git httpd wget -y
-sudo systemctl start httpd
-sudo systemctl enable httpd
+#!/bin/bash
+yum update -y
+yum install git httpd wget unzip -y
+systemctl start httpd
+systemctl enable httpd
+cd /home/ec2-user || cd /root
 wget https://github.com/utrains/static-resume/archive/refs/heads/main.zip
 unzip main.zip
+rm -f /var/www/html/index.html   # 🛠️ Key fix here
 cp -r static-resume-main/* /var/www/html/
+chown -R apache:apache /var/www/html
+rm -rf static-resume-main main.zip
 exit 0
+
+
